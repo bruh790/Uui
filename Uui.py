558ffbf7bbf6cc1931d6bb5519ee278ecfb18f63
bruh790 <133423002+bruh790@users.noreply.github.com>
2023-05-29 15:40:44 +0300

    Create Uui.py

---
+++ Uui.py
@@ -0,0 +1,14 @@
+mport random
+a="azertyuiopqsdfghjklmwxcvbn"
+b="AZERTYUIOPQSDFGHJKLMWXCVBN"
+n="1234567890"
+s="@§%&'(--+/*="
+w=8
+g=a+b+s+n
+x=0
+while True:
+  pas="".join(random.sample(g,w))
+  print("password is: ",pas)
+  x+=1
+  
+  if x==1000000000000000000000000000
